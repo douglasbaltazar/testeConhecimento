e5a745a847004482bfba1a8c92d8e7fbc0a5e85a
unknown <douglasbaltazar1@gmail.com>
2021-10-08 13:09:27 -0300

    Teste de Conhecimento

---
+++ avaliacaoConhecimento/src/avaliacaoConhecimento/Main.java
@@ -0,0 +1,37 @@
+package avaliacaoConhecimento;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		try {
+			int numero = 1;
+			while(numero <= 1){
+				System.out.print("Digite um número: ");
+				numero = scanner.nextInt();
+				if(numero <= 1) {
+					System.out.println("O número deve ser Inteiro e maior que 1.");
+				}
+			}
+			if(isPrimo(numero))
+				System.out.println("Número " + numero + " é primo.");
+			else 
+				System.out.println("Número " + numero + " não é um número primo.");
+		} catch(InputMismatchException ime) {
+			System.out.println("Digite um número valido");
+		}
+	}
+	
+	public static boolean isPrimo(int numero) {
+		for(int i = 2; i < numero; i++) {
+			if(numero % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
